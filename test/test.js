89d27d00adfd1312fde30a3830cfb875ab100d43
Tom Cumming <mail@tomcumming.co.uk>
2018-08-11 12:51:07 +0100

    Fix tests

--- test/test.js
+++ test/test.js
@@ -1,13 +1,13 @@
 const assert = require('assert');
 const cccedict = require('../lib/parse-cc-cedict');
 
-const defs = cccedict.parseFile('test/data/dict.txt', false);
+const defs = cccedict.parseFile('test/data/dict.txt');
 
 assert.deepStrictEqual(
-    defs[4], 
+    defs[4],
     {
         simplified: '有理',
         traditional: '有理',
         pronunciation: 'you3 li3',
-        definitions: [ 'reasonable', 'justified', 'right', '(math.) rational' ] 
+        definitions: [ 'reasonable', 'justified', 'right', '(math.) rational' ]
     });
